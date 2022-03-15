027cceb84886fdae5165ff9e760f78d0f4385b78
Tihon <tihon123123@yandex.ru>
2022-03-15 22:09:48 +0700

    1.1

--- skr.js
+++ skr.js
@@ -6,3 +6,9 @@ else if (age < 17) {div.innerHTML='<h1>Вы подросток</h1>'}
 else if (age > 60) {div.innerHTML='<h1>Вы пожилой</h1>'}
 else {div.innerHTML='<h1>Вы взрослый</h1>'}
 } 
+let arr = []
+const n = 10
+for (let i=0;i<n; i++) {
+	arr[i]=Math.floor(Math.random()*10)
+}
+console.log(arr) 
