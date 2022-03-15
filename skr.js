4c86bc0abae62c80f1347d9532ad5ea29b0770fe
Tihon <tihon123123@yandex.ru>
2022-03-15 22:12:15 +0700

    2.3

--- skr.js
+++ skr.js
@@ -1,26 +1,22 @@
-}
+let students = []
+let current = 0
+let max_id = -1
 
-function next() {
-if (current < students.length) {
-	load_student(current)
-	document.getElementById('btnprev').disabled = false
-	current++
-	load_student(current)
+function LoadFromSite() {
+	$.get('http://217.71.129.139:4035/students.php', function(data){
+@@ -10,6 +10,8 @@ function LoadFromSite() {
+function show_table() {
+	let table = $('#tbl_all')
+	for (let i=0; i < students.length; i++) {
+		if (i > max_id) {
+			max_id = i
+		let tr = $('<tr></tr>')
 
-	if (current == 4) {
-		document.getElementById('btnnext').disabled = true
+		let td1 = $('<td>' + students[i].id + '</td>')
+@@ -19,5 +21,6 @@ function show_table() {
+		tr.append(td1).append(td2).append(td3)
+
+		table.append(tr)
+		}
 	}
-}
-else{
-	document.getElementById('btnnext').disabled = true
-}
-
-function prev() {
-	document.getElementById('btnnext').disabled = false
-	current--
-	load_student(current)
-
-	if (current == 0) {
-		document.getElementById('btnprev').disabled = true
-	}
-}
+} 
