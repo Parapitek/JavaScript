9d9b00a53a5d434e106270990370b826258756c3
Tihon <tihon123123@yandex.ru>
2022-03-15 22:10:26 +0700

    1.3

--- skr.js
+++ skr.js
@@ -1,9 +1,3 @@
-let arr = []
-const n = 10
-for (let i=0;i<n; i++) {
-	arr[i]=Math.floor(Math.random()*10)
-}
-console.log(arr) 
 function show() {
 let age = parseInt(document.getElementById('age').value)
 if (age < 10) {console.log('Вы ребенок')}
@@ -11,3 +5,14 @@ else if (age < 17) {console.log('Вы подросток')}
 else if (age > 60) {console.log('Вы пожилой')}
 else {console.log('Вы взрослый')}
 }  
+let arr = []
+function generate() {
+	let n = parseInt(document.getElementById('age').value)
+for (let i=0; i<n; i++) {
+	arr[i]=Math.floor(Math.random()*10)
+}
+let div = document.getElementById('mes')
+for (let i=0;i<n; i++) {
+	div.innerHTML += arr[i] + ' '
+	}
+} 
